1ff992661f86599f1d4dda11caee5a4d461309aa
TheJoseScream <josealfacr@gmail.com>
2020-05-04 13:59:28 -0600

    implements slides to maps for select agency

--- src/app/agencies-maps/agencies-maps.page.ts
+++ src/app/agencies-maps/agencies-maps.page.ts
@@ -1,5 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
 import { Geolocation } from '@ionic-native/geolocation/ngx';
+import { IonSlides } from '@ionic/angular';
 
 declare var google;
 
@@ -8,39 +9,92 @@ declare var google;
   templateUrl: './agencies-maps.page.html',
   styleUrls: ['./agencies-maps.page.scss'],
 })
+
 export class AgenciesMapsPage implements OnInit {
 
+  @ViewChild('slides', {static: true}) slides: IonSlides;
+
   map = null;
+  currentPosition = {
+    lat: 15.506049,
+    lng: -88.028020
+  };
+  blueDot = null;
+  bestOptionsAgencies = [
+    {name: 'Lito Perez', clientes: 20, lat: 9.976337, lng: -84.839159},
+    {name: 'Centro de Puntarenas', clientes: 2, lat: 9.976835, lng: -84.832266},
+    {name: 'BAC Credomatic', clientes: 15, lat: 9.977691, lng: -84.828158}
+  ];
 
-  constructor(private geolocation: Geolocation) {}
-
-  ngOnInit() {
-    this.loadMap();
-    this.getCurrentPosition();
+  sliderConfig={
+    spaceBetween:10,
+    centeredSlides:true,
+    slidesPerView: 1.8
   }
 
-  getCurrentPosition(){
-    this.geolocation.getCurrentPosition({enableHighAccuracy:true, maximumAge:1000}).then((resp)=>{
-      console.log(resp);
-    }).catch((error)=>{
-      console.log(error)
+  constructor(private geolocation: Geolocation) { }
+
+  async ngOnInit() {
+    await this.getInitialPosition();
+    this.loadMap();
+  }
+
+  async getInitialPosition() {
+    const coords = (await this.geolocation.getCurrentPosition({ enableHighAccuracy: true, maximumAge: 1000 })).coords;
+    this.currentPosition = {
+      lat: coords.latitude,
+      lng: coords.longitude
+    }
+  }
+
+  watchPosition() {
+    this.geolocation.watchPosition({ enableHighAccuracy: true }).subscribe(resp => {
+      this.currentPosition = {
+        lat: resp.coords.latitude,
+        lng: resp.coords.longitude
+      };
+      this.blueDot.setPosition(this.currentPosition);
     });
   }
 
   loadMap() {
     // create a new map by passing HTMLElement
     const mapElement: HTMLElement = document.getElementById('map');
-    // create LatLng object
-    const myLatLng = {lat: 4.658383846282959, lng: -74.09394073486328};
     // create map
     this.map = new google.maps.Map(mapElement, {
-      center: myLatLng,
-      zoom: 12
+      center: this.currentPosition,
+      zoom: 17,
+      disableDefaultUI: true,
+      zoomControl: false,
+      scaleControl: false,
     });
-  
+
     google.maps.event.addListenerOnce(this.map, 'idle', () => {
       mapElement.classList.add('show-map');
     });
+
+    this.blueDot = new google.maps.Marker({
+      position: this.currentPosition,
+      icon: {
+        path: google.maps.SymbolPath.CIRCLE,
+        scale: 8,
+        fillColor: '#1A73E8',
+        fillOpacity: 1.0,
+        strokeColor: '#1A73E8',
+        strokeOpacity: 0.5,
+      },
+      draggable: true,
+      map: this.map
+    });
+
+    this.watchPosition();
   }
+
+  slideChanged(){
+    this.slides.getActiveIndex().then(data=>{
+      console.log(data)
+    })
+  }
+
 
 }
