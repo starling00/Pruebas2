1eaebcc75c0c939f17fb637687bce44e516b3f6f
jairogarcia10 <jaigarcia12@gmail.com>
2020-05-11 09:08:22 -0600

    se agregaron los dropdown

--- src/app/offices/offices.component.ts
+++ src/app/offices/offices.component.ts
@@ -5,6 +5,7 @@ import { CrudService } from '../services/crud.service';
 import { StorageService } from '../services/storage.service';
 import { UtilStorageService } from '../services/util-storage.service';
 import { MenuController } from '@ionic/angular';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-offices',
@@ -12,22 +13,29 @@ import { MenuController } from '@ionic/angular';
   styleUrls: ['./offices.component.scss'],
 })
 export class OfficesComponent implements OnInit {
-
+  urlId: any;
+  ticketServices: any;
   offices: any;
-
+  ticketStatus: any;
+  createdTicket: any;
   constructor(
     private router: Router,
     private params: UtilsService,
     private service: CrudService,
     private storeService: StorageService,
     private localParam: UtilStorageService, 
-    public menuCtrl: MenuController
+    public menuCtrl: MenuController,
+    
   ) { 
     this.menuCtrl.enable(false);
+    
   }
-
+  ngAfterViewInit(){
+    this.GenerateServices();
+  }
   ngOnInit() {
     this.getOffices();
+    
   }
 
   getOffices(){
@@ -43,5 +51,46 @@ export class OfficesComponent implements OnInit {
     this.storeService.localSave(this.localParam.localParam.ticketOffice, id);
     this.router.navigateByUrl('/heart-rate/'+id);
   }
+  customActionSheetOptions: any = {
+    header: 'Colors',
+    subHeader: 'Select your favorite color'
+  };
+  getTicketStatus(visitId){
+    this.service.get(this.params.params.ticketStatus+'/'+visitId).subscribe((resp) => {
+      this.ticketStatus = resp;
+      this.storeService.localSave(this.localParam.localParam.ticketStatus, this.ticketStatus);
 
-}
+      console.log(this.ticketStatus);
+    }, (err) => {
+      console.error(err);
+    });
+  }
+
+  createTicket(){
+    this.service.saveTicket(this.params.params.ticketCreate+'/serviceId/'+20+'/officeId/'+ 9, null).subscribe((resp) => {
+      this.createdTicket = resp;
+      this.storeService.localSave(this.localParam.localParam.createdTicket, this.createdTicket);
+
+      this.getTicketStatus(this.createdTicket.visitId);
+      console.log(this.createdTicket);
+    }, (err) => {
+      console.error(err);
+    });
+  }
+ 
+
+  GenerateServices(){
+    this.service.saveTicket(this.params.params.ticketServices+'/'+9, null).subscribe((resp) => {
+      this.ticketServices = resp;
+      this.storeService.localSave(this.localParam.localParam.ticketServices, this.ticketServices);
+
+      console.log(this.ticketServices);
+    }, (err) => {
+      console.error(err);
+    });
+  }
+  go() {
+    this.createTicket();
+    this.router.navigateByUrl('/menu/first/tabs/tab2');
+  }
+}// fin d la class
